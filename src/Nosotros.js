4d911ac0b02fad4863e9e19a32b3a6b52aa9b6ad
Julio Serra <julioserra@iMac-de-Julio.local>
2022-01-13 17:56:42 +0100

    update title nosotros.js

--- src/Nosotros.js
+++ src/Nosotros.js
@@ -5,7 +5,7 @@ class Nosotros extends React.Component {
 
     render() {
         return <div className="max-w-7xl mx-auto px-4 sm:px-6 mt-6">
-            <section><h1>Sobre Nosotros</h1></section>
+            <section><h1>Titulo Sobre Nosotros</h1></section>
             <section>Contenido</section>
             <footer>Footer</footer>
         </div>
