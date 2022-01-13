463f12d38879a578a0ba00e6003138812f2664e4
Julio Serra <julioserra@iMac-de-Julio.local>
2022-01-13 17:17:39 +0100

    my first commit

---
+++ src/Nosotros.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+class Nosotros extends React.Component {
+
+
+    render() {
+        return <div className="max-w-7xl mx-auto px-4 sm:px-6 mt-6">
+            <section><h1>Sobre Nosotros</h1></section>
+            <section>Contenido</section>
+            <footer>Footer</footer>
+        </div>
+    }
+
+}
+
+export default Nosotros;
